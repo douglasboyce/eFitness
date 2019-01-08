e3bdce95f5ce2c3185fc76f0c91611ecc32f1ea7
dboyce77 <dboyce77@gmail.com>
2019-01-08 11:33:45 -0600

    dashboard

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function Dashboard() {
+  return (
+    <div>
+
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <h1 className="text-center">myDashboard</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-6">
+          <h3>Personal Information</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
+              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
+              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
+              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
+              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
+              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
+              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
+              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
+              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+            </p>
+          </Col>
+          <Col size="md-6">
+          <h3>Exercise Log</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
+              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
+              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
+              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
+              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
+              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
+              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
+              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
+              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-6">
+          <hr/>
+          <h3>BP Chart</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
+              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
+              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
+              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
+              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
+              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
+              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
+              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
+              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+            </p>
+          </Col>
+          <Col size="md-6">
+          <hr />
+          <h3>Fitness / Outdoor Meet-Up</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
+              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
+              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
+              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
+              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
+              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
+              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
+              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
+              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+            </p>
+          </Col>
+        </Row>
+        </Container>
+
+
+    </div>
+  );
+}
+
+export default Dashboard;
